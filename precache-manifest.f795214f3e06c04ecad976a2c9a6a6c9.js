e8ef5ebfcb08b1aa60a3951a60fc86e7aed0472d
Junaid <junaid1460@gmail.com>
2020-07-06 13:02:34 +0530

    Updates

---
+++ precache-manifest.f795214f3e06c04ecad976a2c9a6a6c9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3cdf736f65ae2b2f964f6544ee96792a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "863c6da452d5cf3d8509",
+    "url": "/static/css/main.1a20dfa3.chunk.css"
+  },
+  {
+    "revision": "380282ca77a54d7859a7",
+    "url": "/static/js/2.2db1f785.chunk.js"
+  },
+  {
+    "revision": "863c6da452d5cf3d8509",
+    "url": "/static/js/main.cc46e1cb.chunk.js"
+  },
+  {
+    "revision": "aa1d0f1c672be7083a94",
+    "url": "/static/js/runtime~main.afeea630.js"
+  },
+  {
+    "revision": "2be7a61a1b41e2d88c3130948efad4a3",
+    "url": "/static/media/logo.2be7a61a.svg"
+  }
+]);
