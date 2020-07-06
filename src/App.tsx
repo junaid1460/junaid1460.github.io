d3f75a7f8eb70b3cea854ab9da6aab44d96d7f20
Junaid <junaid1460@gmail.com>
2020-07-06 16:11:53 +0530

    add simple mystery button

--- src/App.tsx
+++ src/App.tsx
@@ -3,21 +3,8 @@ import './App.css';
 
 function App() {
   return (
-    <div class="App">
-      <header class="App-header">
-        <img src={logo} class="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          class="App-link"
-          href="https://github.com/ryansolid/solid"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Solid
-        </a>
-      </header>
+    <div class="container">
+        <button class="action-button" disabled={true}> mystery </button>
     </div>
   );
 }
