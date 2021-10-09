282c5a53df65ff9d0182d5214f27a915cba87627
Junaid <junaid1460@gmail.com>
2021-10-09 18:32:36 +0530

    feat: add font awesome free version

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,8 @@
 import { render } from "solid-js/web";
 
 import "./index.css";
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import "@fortawesome/fontawesome-free/js/all.min.js";
 import App from "./app";
 
 render(() => <App />, document.getElementById("root")!);
